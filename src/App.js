c349484f41c04b73ae227c699e8d5dd96b2db1c3
suryawansh <suryawanshi17298@gmail.com>
2023-03-01 18:36:41 +0530

    - updated base url

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
+    <Router basename="cat_api_project">
       <Switch>
         <Route exact path="/">
           <Home />
